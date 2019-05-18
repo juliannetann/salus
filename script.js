5e969366b59c48d023293d6c95f5a22fdaf92f05
LC4311 <16984757+LC4311@users.noreply.github.com>
2019-05-18 10:47:06 +0800

    fixed media queries

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-button1 = document.getElementsByClassName("button")
+// button1 = document.getElementsByClassName("button")
 
-document.addEventListener("onDOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("Loaded")
+    let windowWidth = document.body.clientWidth
+    let buttons = document.querySelectorAll(".btn.btn-primary.more")
+    buttons = Array.from(buttons)
+    let cards = document.getElementsByClassName("card")
+    cards = Array.from(cards)
 
-    document.getElementById()
+    function searchClass(elemArray, className) {
+        return elemArray.map( elem => {
+            return Array.from(elem.classList).includes(className)
+        })
+    }
+
+    
+    if (windowWidth < 576 ){
+        
+        buttons.map( button => {
+            button.classList.remove("mt-auto")
+            button.classList.remove("more")
+        })
+
+        cards.map( card => {
+            try{
+                card.classList.remove("card-1")
+                card.classList.remove("card-3")
+            } catch (e) {
+                console.log(e)
+            }
+        })
+
+        
+        console.log(buttons)
+    } else if (windowWidth > 576 && (searchClass(buttons,"more").includes(false) || searchClass(buttons,"mt-auto").includes(false)) ){
+        
+        
+        buttons.map( button => {
+            button.classList.add("mt-auto")
+            button.classList.add("more")
+        })
+        
+        console.log(buttons)
+    }
 
 })
