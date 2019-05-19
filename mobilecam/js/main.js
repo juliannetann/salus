399d6596d08ba508efc44d2811355693ea84ec8b
LC4311 <16984757+LC4311@users.noreply.github.com>
2019-05-19 10:37:46 +0800

    added chatbot features 2

--- mobilecam/js/main.js
+++ mobilecam/js/main.js
@@ -38,10 +38,10 @@ AWS.config.update({
 
 var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-dynamodb.batchGetItem(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
+// dynamodb.batchGetItem(params, function (err, data) {
+//   if (err) console.log(err, err.stack); // an error occurred
+//   else     console.log(data);           // successful response
+// });
 // var s3 = new AWS.S3({
 //   apiVersion: '2006-03-01',
 //   params: {Bucket: albumBucketName}
